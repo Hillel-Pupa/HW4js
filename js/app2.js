98d03aab0a546d4933ef28647e17214ff3c08604
Anna <protsenko.anyta@gmail.com>
2021-10-12 23:14:53 +0200

    fix 2 hw

--- js/app2.js
+++ js/app2.js
@@ -3,90 +3,73 @@ let myArr = [
   76, -4, 12, -35, 4, 47,
 ];
 document.write("<p>" + myArr + "</p>");
-let arrPositiv = [];
-for (let index = 0; index < myArr.length; index++) {
-  let element = myArr[index];
-  if (element > 0) {
-    arrPositiv.push(element);
-  }
-}
-let arrPositivCount = arrPositiv.length;
+
+let arrPositivCount = 0;
 let arrPositivSum = 0;
-for (let index = 0; index < arrPositiv.length; index++) {
-  arrPositivSum = arrPositivSum + arrPositiv[index];
-}
-document.write("<p> positiv arr count is " + arrPositivCount + "</p>");
-document.write("positiv sum of arr is " + arrPositivSum);
 let minIndex = 0;
-for (let index = 0; index < myArr.length; index++) {
-  let element = myArr[index];
-  if (myArr[minIndex] > element) {
-    minIndex = index;
-  }
-}
-document.write(
-  "<p>min index of min element is " +
-    minIndex +
-    "</p>" +
-    "<p> min element is " +
-    myArr[minIndex] +
-    "</p>"
-);
 let maxIndex = 0;
-for (let index = 0; index < myArr.length; index++) {
-  let element = myArr[index];
-  if (myArr[maxIndex] < element) {
-    maxIndex = index;
-  }
-}
-document.write(
-  "<p>max index of max element is " +
-    maxIndex +
-    "</p>" +
-    "<p> max element is " +
-    myArr[maxIndex] +
-    "</p>"
-);
-let arrNegativ = [];
-for (let index = 0; index < myArr.length; index++) {
-  let element = myArr[index];
-  if (element < 0) {
-    arrNegativ.push(element);
-  }
-}
-let arrNegativCount = arrNegativ.length;
-document.write("<p> negativ arr count is " + arrNegativCount + "</p>");
+let arrNegativCount = 0;
 let arrPositivCountOdd = 0;
 let arrPositivCountEven = 0;
 let arrPositivSumOdd = 0;
 let arrPositivSumEven = 0;
-for (let index = 0; index < arrPositiv.length; index++) {
-  if (!(arrPositiv[index] % 2 === 0)) {
-    arrPositivCountOdd++;
-    arrPositivSumOdd = arrPositivSumOdd + arrPositiv[index];
-  } else {
-    arrPositivCountEven++;
-    arrPositivSumEven = arrPositivSumEven + arrPositiv[index];
-  }
-}
-document.write(
-  "<p> count of positiv arr odd is " + arrPositivCountOdd + "</p>"
-);
-document.write(
-  "<p> count of positiv arr even is " + arrPositivCountEven + "</p>"
-);
-document.write("<p> sum of positiv arr even is " + arrPositivSumEven + "</p>");
-document.write("<p> sum of positiv arr odd is " + arrPositivSumOdd + "</p>");
 let arrPositivMult = 1;
-for (let index = 0; index < arrPositiv.length; index++) {
-  arrPositivMult = arrPositivMult * arrPositiv[index];
-}
-document.write(
-  "<p> multiplication of arr positiv is " + arrPositivMult + "</p>"
-);
+let myArrOutput = [];
 for (let index = 0; index < myArr.length; index++) {
-  if (index !== maxIndex) {
-    myArr[index] = 0;
+  let element = myArr[index];
+  // Найти сумму и количество положительных элементов.
+  if (element > 0) {
+    arrPositivCount++;
+    arrPositivSum += element;
+    // Найти произведение положительных элементов.
+    arrPositivMult *= element;
+    // Найти количество нечетных положительных элементов.
+    // Найти сумму нечетных положительных элементов.
+    if (!(element % 2 === 0)) {
+      arrPositivCountOdd++;
+      arrPositivSumOdd += element;
+    } // Найти количество четных положительных элементов.
+    // Найти сумму четных положительных элементов.
+    else {
+      arrPositivCountEven++;
+      arrPositivSumEven += element;
+    }
+  }
+  // Найти минимальный элемент массива и его порядковый номер.
+  if (myArr[minIndex] > element) {
+    minIndex = index;
+  }
+  // Найти максимальный элемент массива и его порядковый номер.
+  if (myArr[maxIndex] < element) {
+    maxIndex = index;
+  }
+  // Определить количество отрицательных элементов.
+  if (element < 0) {
+    arrNegativCount++;
   }
 }
-document.write(myArr);
+for (let index = 0; index < myArr.length; index++) {
+  let element = myArr[index];
+  // Найти наибольший среди элементов массива, остальные обнулить.
+  if (index !== maxIndex) {
+    myArrOutput.push(0);
+  } else {
+    myArrOutput.push(element);
+  }
+}
+
+document.write(
+  ` <p> positiv arr count is ${arrPositivCount} </p> 
+    <p>positiv sum of arr is ${arrPositivSum} </p>
+    <p> index of min element is ${minIndex} </p> 
+    <p> min element is ${myArr[minIndex]} </p> 
+    <p> index of max element is ${maxIndex} </p> 
+    <p> max element is ${myArr[maxIndex]} </p> 
+    <p> negativ arr count is ${arrNegativCount} </p> 
+    <p> count of positiv arr odd is ${arrPositivCountOdd} </p> 
+    <p> count of positiv arr even is ${arrPositivCountEven} </p> 
+    <p> sum of positiv arr even is ${arrPositivSumEven} </p> 
+    <p> sum of positiv arr odd is ${arrPositivSumOdd} </p> 
+    <p> multiplication of arr positiv is ${arrPositivMult} </p> 
+    <p> ${myArrOutput} </p>`
+);
